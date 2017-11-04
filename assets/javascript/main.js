dc1f6824875bc7c1dcc3e93f92feb828a0461c66
pamula <prathima.amula@gmail.com>
2017-11-04 08:27:23 -0700

    message

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,173 @@
-$(document).ready(function () {
-    console.log('hello world');
 
-    // global variables
-    var meta = JSON.parse('https://epic.gsfc.nasa.gov/api/natural');
+function searchTemp() {
+
+  //Empty the html giphy-area after each search
+
+  $("#giphy-area").html("");
+
+  searchValue = $("#search").val().trim();
+
+  //Set queryURL for AJAX Request
+
+  var queryURL= "https://api.giphy.com/v1/gifs/search?q="+ searchValue +"&api_key=XTD2QIleof4xLyh8zHWCGfA1OExJXaGZ&limit=10";
+
   
-  var queryURL= "https://api.giphy.com/v1/gifs/search?q="+ giphy.searchValue +"&api_key=XTD2QIleof4xLyh8zHWCGfA1OExJXaGZ&limit=10";
 
-    $.ajax('https://epic.gsfc.nasa.gov/api/natural', { 
-        success : function(iDataArr, stat, xhr) { 
-            // do something with the list
-        }
-    });
+
+ // https://images-api.nasa.gov  /search?q={q}
+  //AJAX Request
+
+  $.ajax({
+
+    url: queryURL,
+
+    method: 'GET',
+
+}).done((response) => {
+
+    console.log(response);
+
+    for(i = 0; i < response.data.length; i++){
+
+      //Add raiting and img to html
+
+      $("#giphy-area").append("<div class= 'gif-div'>Rating: " + response.data[i].rating.toUpperCase() +
+       "<br>" + "<img data-name= " + response.data[i].images.original.url + " src= " + response.data[i].images.original_still.url
+       + " class= 'gif-img'></div>");
+
+  };
+
+});
+};
+
+function search() {
+
+  //Empty the html giphy-area after each search
+
+  $("#giphy-area").html("");
+
+  searchValue = $("#search").val().trim();
+  console.log(searchValue);
+
+  pressTopicBtnGiphy(searchValue);
+  pressTopicBtnBing(searchValue);
+
+  //Clear #search
+
+  $("#search").val("")
+
+  //Run init()
+
+  // init();
+
+};
+
+function pressTopicBtnGiphy (searchValue) {
+
+ var queryURL= "https://api.giphy.com/v1/gifs/search?q="+ searchValue +"&api_key=XTD2QIleof4xLyh8zHWCGfA1OExJXaGZ&limit=10";
+
+ $.ajax({
+
+    url: queryURL,
+
+    method: 'GET'
+
+}).done((response) => {
+
+    console.log(response);
+
+    for(i = 0; i < response.data.length; i++){
+
+      //Add rating and img to html
+
+      $("#giphy-area").append("<div class= 'gif-div'>Rating: " + response.data[i].rating.toUpperCase() 
+        + "<br>" + "<img data-name= " + response.data[i].images.original.url + " src= " + response.data[i].images.original.url
+        + " class= 'gif-img'></div>");
+
+  };
+
+});
+
+};
+
+function pressTopicBtnBing (searchValue) {
+
+   var queryURL= "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q="+ searchValue +"&count=10";
+
+
+   $.ajax({
+
+    url: queryURL,
+
+    beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","d18f5965cced4c3e82a8548bd6dab528"); //replace value with your own key
+            },
+
+
+            method: 'GET'
+
+        }).done((response) => {
+
+            console.log(response);
+
+            for(i = 0; i < response.value.length; i++){
+
+      //Add rating and img to html
+
+      $("#giphy-area").append("<div class= 'gif-div'><img data-name= " + response.value[i].name + " src= " + response.value[i].contentUrl
+        + " class= 'bing-img'></div>");
+
+  };
+
+});
+
+    };
+
+
+
+//In creating each image, I added a data-name containing the gif url. Here I swap that with the still image url being used in the src.
+
+function changeImage() {
+
+    var temp = $(this).attr("data-name");
+
+    $(this).attr("data-name", $(this).attr("src"));
+
+    $(this).attr("src", temp);
+
+};
+
+
+
+
+
+
+
+//=======================
+
+//MAIN PROCESS
+
+//=======================
+
+//Initialize on start
+
+// init();
+
+
+
+//When the Submit button is clicked the search function is called
+
+//$("#search-btn").on("click", search);
+$("#search-btn").on("click", search);
+
+//When the Country buttons are clicked, the presstopicBtn function is called
+
+$(document).on("click", ".topic-btn", pressTopicBtn);
+
+//When the gif images are clicked, changeImage function is called
+
+$(document).on("click", ".gif-img", changeImage);
 
 
     // functions___________________________________________________________________________
@@ -44,6 +201,5 @@ $(document).ready(function () {
     // listeners end here ____________________________________________________________________
 
 
-    // start main process___________________________________________________________________________
+  // start main process___________________________________________________________________________
 
-});
---
+++ notes.md
@@ -0,0 +1,4 @@
+GET https://images-api.nasa.gov  /search?q={q}
+GET /search?q={q}
+Performing a search
+unsplash api
